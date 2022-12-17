5b5ef90d3b8f63417ceb88fd2f28dabd19a75fb1
codejester <codejester@protonmail.ch>
2022-12-17 16:45:45 -0500

    Updated code formatting

--- src/webook.server/app.py
+++ src/webook.server/app.py
@@ -2,19 +2,22 @@ from flask import Flask, request, json
 
 app = Flask(__name__)
 
-@app.route('/webhook', methods=['POST'])
+
+@app.route("/webhook", methods=["POST"])
 def webhook():
-    #print(f'Request: {request.json}')
-    if request.method == 'POST':
+    # print(f'Request: {request.json}')
+    if request.method == "POST":
         print("Data received from Webhook is: ", request.json)
         return "Webhook received!"
 
-@app.route('/event', methods=['POST'])
+
+@app.route("/event", methods=["POST"])
 def event():
-    #print(f'Request: {request.json}')
-    if request.method == 'POST':
+    # print(f'Request: {request.json}')
+    if request.method == "POST":
         print("Data received from Event is: ", request.json)
         return "Event received!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
--- src/webook.server/tester.py
+++ src/webook.server/tester.py
@@ -1,45 +1,52 @@
 import json
 import requests
 
+
 def webhook():
-  url = 'http://127.0.0.1:8000/webhook'
+    url = "http://127.0.0.1:8000/webhook"
 
-  payload = {
-    'FirstName': 'First',
-    'LastName': 'Name',
-    'Email': 'name@email.com',
-  }
+    payload = {
+        "FirstName": "First",
+        "LastName": "Name",
+        "Email": "name@email.com",
+    }
 
-  response = requests.post(url, data=json.dumps(payload), headers={'Content-Type': 'application/json'});
+    response = requests.post(
+        url, data=json.dumps(payload), headers={"Content-Type": "application/json"}
+    )
 
-  if response.status_code != 200:
-    raise Exception('Not OK (non-200 status code received)')
+    if response.status_code != 200:
+        raise Exception("Not OK (non-200 status code received)")
 
-  response = response.text;
+    response = response.text
 
-  print(f'Response: {response}')
+    print(f"Response: {response}")
+
 
 def event():
-  url = 'http://127.0.0.1:8000/event'
+    url = "http://127.0.0.1:8000/event"
 
-  payload = {
-    'event': 'event-update',
-    'date': '2022-08-23',
-    'clearence': {
-      'min': '200',
-      'max': '800',
-    },
-  }
+    payload = {
+        "event": "event-update",
+        "date": "2022-08-23",
+        "clearence": {
+            "min": "200",
+            "max": "800",
+        },
+    }
 
-  response = requests.post(url, data=json.dumps(payload), headers={'Content-Type': 'application/json'});
+    response = requests.post(
+        url, data=json.dumps(payload), headers={"Content-Type": "application/json"}
+    )
 
-  if response.status_code != 200:
-    raise Exception('Not OK (non-200 status code received)')
+    if response.status_code != 200:
+        raise Exception("Not OK (non-200 status code received)")
 
-  response = response.text;
+    response = response.text
 
-  print(f'Response: {response}')
+    print(f"Response: {response}")
 
-if __name__ == '__main__':
-  webhook()
-  event()
+
+if __name__ == "__main__":
+    webhook()
+    event()
